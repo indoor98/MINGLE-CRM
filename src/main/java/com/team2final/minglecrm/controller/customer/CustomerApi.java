90411c9bba77aacd984a4e7c52506d36621a430f
dongwook <hsw50108@gmail.com>
2024-05-31 19:17:35 +0900

    Remove : Customer api 주석

--- src/main/java/com/team2final/minglecrm/controller/customer/CustomerApi.java
+++ src/main/java/com/team2final/minglecrm/controller/customer/CustomerApi.java
@@ -5,6 +5,7 @@ import com.team2final.minglecrm.service.customer.CustomerService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,42 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerApi {
     private final CustomerService customerService;
 
-    // 고객 전체 List
     // TODO : paging 처리
     @GetMapping()
+    @PreAuthorize("hasRole('ROLE_STAFF')")
     public ResponseEntity<List<CustomerResponse>> getAllCustomers() {
         List<CustomerResponse> customers = customerService.getAllCustomer();
         return ResponseEntity.ok(customers);
     }
 
-    // 고객 상세 조회 페이지
-//    @GetMapping()
-
-//    @GetMapping("/employee/{employee}")
-//    public ResponseEntity<List<CustomerResponse>> getCustomersByUserId(
-//            @PathVariable("employee") String employeeName) {
-//        List<CustomerResponse> customersByUserId = customerService.getCustomersByUserId(
-//                employeeName);
-//        return ResponseEntity.ok(customersByUserId);
-//    }
-//
-//    @GetMapping("/group/{customerGroup}")
-//    public ResponseEntity<List<CustomerResponse>> getCustomersByCustomerGroup(
-//            @PathVariable("customerGroup") String customerGroup) {
-//        List<CustomerResponse> customersByGroup = customerService.getCustomersByCustomerGroup(
-//                customerGroup);
-//        return ResponseEntity.ok(customersByGroup);
-//    }
-//
-//    @PatchMapping("/{customerId}")
-//    public ResponseEntity<Void> updateCustomer(@PathVariable("customerId") Long Id,
-//            @RequestBody CustomerUpdateRequest customerUpdateRequest) {
-//        customerService.updateCustomer(Id, customerUpdateRequest);
-//        return ResponseEntity.ok().build();
-//    }
-//
-//    @GetMapping("/test")
-//    public ResponseEntity<Void> test() {
-//        return ResponseEntity.ok().build();
-//    }
 }
--- src/main/java/com/team2final/minglecrm/service/customer/CustomerService.java
+++ src/main/java/com/team2final/minglecrm/service/customer/CustomerService.java
@@ -5,21 +5,24 @@ import com.team2final.minglecrm.controller.customer.response.CustomerResponse;
 import com.team2final.minglecrm.entity.customer.Customer;
 import com.team2final.minglecrm.persistence.repository.customer.CustomerRepository;
 import com.team2final.minglecrm.persistence.repository.employee.EmployeeRepository;
+
 import java.util.List;
 import java.util.stream.Collectors;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
     private final EmployeeRepository employeeRepository;
 
     // 고객 조회
-    @Transactional
+    @Transactional(readOnly = true)
     public List<CustomerResponse> getAllCustomer() {
         return customerRepository.findAll().stream()
                 .map(customer -> new CustomerResponse(
@@ -44,7 +47,7 @@ public class CustomerService {
             throw new RuntimeException("없슴둥");
         }
 
-        CustomerResponse customerResponse = CustomerResponse.builder()
+        return CustomerResponse.builder()
                 .id(customer.getId())
                 .address(customer.getAddress())
                 .amount(customer.getReward().getAmount())
@@ -56,75 +59,17 @@ public class CustomerService {
                 .name(customer.getName())
                 .gender(customer.getGender())
                 .build();
-
-        return customerResponse;
     }
 
-    @Transactional
     public void updateCustomer(Long customerId, CustomerUpdateRequest customerUpdateRequest) {
         Customer customer = customerRepository.findById(customerId).orElseThrow();
         customer.updateCustomerDetail(customerUpdateRequest);
 
     }
 
-    @Transactional
     public void deleteCustomer(Long customerId) {
         Customer customer = customerRepository.findById(customerId).orElseThrow();
         customer.deleteCustomer();
     }
 
-    // 담당자별 고객 조회
-//    @Transactional
-//    public List<CustomerResponse> getCustomersByUserId(String employee) {
-//        return customerRepository.findByEmployeeName(employee).stream()
-//                .map(customer -> new CustomerResponse(
-//                        customer.getId(),
-//                        customer.getName(),
-//                        customer.getGrade(),
-//                        customer.getPhone(),
-//                        customer.getAddress(),
-//                        customer.getEmployee().getName(),
-//                        customer.getMemo(),
-//                        customer.getGender(),
-//                        customer.getBirth()
-//                ))
-//                .collect(Collectors.toList());
-//    }
-//
-//    // 고객 그룹별 고객 조회
-//    @Transactional
-//    public List<CustomerResponse> getCustomersByCustomerGroup(String customerGroup) {
-//        return customerRepository.findByGrade(customerGroup).stream()
-//                .map(customer -> new CustomerResponse(
-//                        customer.getId(),
-//                        customer.getName(),
-//                        customer.getGrade(),
-//                        customer.getPhone(),
-//                        customer.getAddress(),
-//                        customer.getEmployee().getName(),
-//                        customer.getMemo(),
-//                        customer.getGender(),
-//                        customer.getBirth()
-//                ))
-//                .collect(Collectors.toList());
-//    }
-//
-//    // 고객 정보 수정
-//    @Transactional
-//    public void updateCustomer(Long id, CustomerUpdateRequest param) {
-//        Customer customer = customerRepository.findById(id).orElseThrow();
-//
-////        customer.setName(param.getName());
-////        customer.setGrade(param.getGrade());
-////        customer.setPhone(param.getPhone());
-////        customer.setAddress(param.getAddress());
-////        customer.setMemo(param.getMemo());
-////        customer.setGender(param.getGender());
-//
-//        Employee employee = employeeRepository.findById(customer.getEmployee().getId()).orElseThrow();
-//        employee.setName(param.getEmployeeName());
-//
-//        customerRepository.save(customer);
-//        employeeRepository.save(employee);
-//    }
 }
