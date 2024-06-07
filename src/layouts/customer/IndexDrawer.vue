c381d238653424aeb1f2c4609f8c5cd86b40dc62
dongwook <hsw50108@gmail.com>
2024-06-07 14:28:04 +0900

    feat : customer 초기 세팅

---
+++ src/layouts/customer/IndexDrawer.vue
@@ -0,0 +1,60 @@
+<template>
+  <q-drawer
+    v-model="drawer"
+    :width="240"
+    bordered
+    :mini="$q.screen.lt.md"
+    :breakpoint="$q.screen.sizes.sm"
+  >
+    <q-scroll-area class="fit">
+      <q-list>
+        <template v-for="(menuItem, index) in menuList1" :key="index">
+          <q-item clickable :active="menuItem.label === 'Outbox'" v-ripple>
+            <q-item-section avatar>
+              <q-icon :name="menuItem.icon" />
+            </q-item-section>
+            <q-item-section>
+              {{ menuItem.label }}
+            </q-item-section>
+          </q-item>
+          <q-separator :key="'sep' + index" v-if="menuItem.separator" />
+        </template>
+      </q-list>
+    </q-scroll-area>
+  </q-drawer>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const drawer = ref(true);
+const menuList1 = [
+  {
+    icon: 'person',
+    label: '고객',
+    separator: false,
+  },
+  {
+    icon: 'calendar_today',
+    label: '예약',
+    separator: false,
+  },
+  {
+    icon: 'chat',
+    label: '상담',
+    separator: false,
+  },
+  {
+    icon: 'attach_money',
+    label: '판매',
+    separator: false,
+  },
+  {
+    icon: 'insert_chart',
+    label: '통계',
+    separator: true,
+  },
+];
+</script>
+
+<style lang="scss" scoped></style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,17 +2,22 @@ const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
+  {
+    path: '/mingle',
+    component: () => import('layouts/customer/Index.vue'),
+    children: [
+      { path: '', component: () => import('pages/customer/IndexPage.vue') },
+    ],
+  },
+];
 
-export default routes
+export default routes;
