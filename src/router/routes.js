d703902dcb933a3815cfc7fe74e0b66c89883d3c
dongwook <hsw50108@gmail.com>
2024-06-09 16:38:12 +0900

    feat : customer detail

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,6 +6,7 @@ const routes = [
       { path: "", component: () => import("pages/IndexPage.vue") },
       { path: "customer", component: () => import("pages/CustomerPage.vue") },
       { path: "review", component: () => import("pages/ReviewPage.vue") },
+
     ],
   },
 
@@ -20,6 +21,7 @@ const routes = [
     component: () => import("layouts/customer/Index.vue"),
     children: [
       { path: "", component: () => import("pages/customer/IndexPage.vue") },
+      { path: "/customer-detail/:id", component: () => import("pages/customer/CustomerDetailPage.vue") }
     ],
   },
 ];
