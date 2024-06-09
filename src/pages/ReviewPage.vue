4bab9c0b508d26dd3b7e29eb8f5f78918a3a8e8a
indoor98 <98157935+indoor98@users.noreply.github.com>
2024-06-09 19:29:48 +0900

    Hotel Review 페이징 처리

--- src/pages/ReviewPage.vue
+++ src/pages/ReviewPage.vue
@@ -30,43 +30,117 @@
         <template v-slot:hint> Field hint </template>
       </q-input>
 
-      <div class="col">
-        <q-scroll-area style="height: 200px; max-width: 300px">
-          <div v-for="n in 100" :key="`first-${n}`" class="q-py-xs">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </div>
-        </q-scroll-area>
+      <div class="col-4 q-pa-md scroll" style="max-height: 200px">
+        <div v-for="n in 100" :key="`second-${n}`" class="q-py-xs">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </div>
       </div>
-      <!-- Second Scroll Area -->
-      <div class="col">
-        <q-scroll-area style="height: 200px; max-width: 300px">
-          <div v-for="n in 100" :key="`second-${n}`" class="q-py-xs">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </div>
-        </q-scroll-area>
+      <div class="col-4 q-pa-md scroll" style="max-height: 200px">
+        <div v-for="n in 100" :key="`second-${n}`" class="q-py-xs">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </div>
       </div>
     </section>
 
     <section class="q-mt-xl">
       <div class="row q-col-gutter-xl">
-        <div v-for="n in 9" :key="n" class="col-12 col-sm-6 col-md-4 col-lg-4">
+        <div
+          v-for="(review, index) in reviews"
+          :key="index"
+          class="col-12 col-sm-6 col-md-4 col-lg-4"
+        >
           <q-card class="my-card">
-            <q-img ratio="1" src="https://cdn.quasar.dev/img/parallax2.jpg">
-              <div
-                class="absolute-bottom text-subtitle2 flex flex-center full-height"
-              >
-                Title
+            <q-card-section>
+              <div class="row">
+                <div calss="col">
+                  <div class="q-mb-sm">{{ review.roomType }}</div>
+                  <div class="q-mb-sm">{{ review.customerName }}</div>
+                  <div>{{ review.createdTime.substring(0, 10) }}</div>
+                </div>
+                <div class="col q-px-lg">
+                  <div>친절도</div>
+                  <q-rating
+                    size="15px"
+                    v-model="review.kindnessRating"
+                    :max="5"
+                    color="primary"
+                  />
+                  <div>청결도</div>
+                  <q-rating
+                    size="15px"
+                    v-model="review.cleanlinessRating"
+                    :max="5"
+                    color="primary"
+                  />
+                </div>
+                <div class="col">
+                  <div>편의성</div>
+                  <q-rating
+                    size="15px"
+                    v-model="review.convenienceRating"
+                    :max="5"
+                    color="primary"
+                  />
+                  <div>위치 만족도</div>
+                  <q-rating
+                    size="15px"
+                    v-model="review.locationRating"
+                    :max="5"
+                    color="primary"
+                  />
+                </div>
               </div>
-            </q-img>
+            </q-card-section>
+
+            <q-card-section class="scroll" style="max-height: 300px">
+              {{ review.comment }}
+              {{ review.createdTime }}
+            </q-card-section>
           </q-card>
         </div>
       </div>
     </section>
+    <section class="flex flex-center q-mt-xl">
+      <q-pagination
+        v-model="current"
+        max="5"
+        direction-links
+        flat
+        color="grey"
+        active-color="primary"
+      />
+    </section>
   </q-page>
 </template>
+<script setup>
+import { ref, watch, onMounted } from "vue";
+import axios from "axios"; // axios 모듈을 기본 내보내기로 임포트
 
-<script setup></script>
+const current = ref(1);
+const reviews = ref([]);
+
+const getHotelReviews = async () => {
+  try {
+    const response = await axios.get(
+      `http://localhost:8080/api/hotel/reviews/${current.value - 1}`
+    );
+    reviews.value = response.data.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// 페이지네이션 값이 변경될 때마다 getHotelReviews 함수 호출
+watch(current, () => {
+  getHotelReviews();
+});
+
+// 컴포넌트가 마운트될 때 getHotelReviews 함수 호출
+onMounted(() => {
+  getHotelReviews();
+});
+</script>
 
 <style lang="scss" scoped></style>
