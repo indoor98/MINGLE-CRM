0106e4e8757aaf1512c2f474d1d053f3c74132d4
chan-nni <rkdcksal02@gmail.com>
2024-06-14 22:04:30 +0900

    조치 내용 있는 문의 목록 조회

--- src/pages/inquiry/InquiryPage.vue
+++ src/pages/inquiry/InquiryPage.vue
@@ -16,13 +16,13 @@
       :pagination="pagination"
       @row-click="onRowClick"
     />
-    <UnansweredInquiryList
-      v-if="selectedOption.value === 'unanswered'"
+    <AnsweredInquiryList
+      v-if="selectedOption.value === 'answered'"
       :pagination="pagination"
       @row-click="onRowClick"
     />
-    <AnsweredInquiryList
-      v-if="selectedOption.value === 'answered'"
+    <UnansweredInquiryList
+      v-if="selectedOption.value === 'unanswered'"
       :pagination="pagination"
       @row-click="onRowClick"
     />
@@ -37,16 +37,16 @@
 <script setup>
 import { ref } from "vue";
 import InquiryList from "pages/inquiry/InquiryList.vue";
-import UnansweredInquiryList from "pages/inquiry/UnansweredInquiryList.vue";
 import AnsweredInquiryList from "pages/inquiry/AnsweredInquiryList.vue";
+import UnansweredInquiryList from "pages/inquiry/UnansweredInquiryList.vue";
 import InquiryWithActionList from "pages/inquiry/InquiryWithActionList.vue";
 
 const pagination = ref({ page: 1, rowsPerPage: 10, rowsNumber: 0 });
 const selectedOption = ref({ label: "전체 문의", value: "all" });
 const options = [
   { label: "전체 문의", value: "all" },
-  { label: "답변 없는 문의", value: "unanswered" },
   { label: "답변 있는 문의", value: "answered" },
+  { label: "답변 없는 문의", value: "unanswered" },
   { label: "조치 내용 있는 문의", value: "withaction" },
 ];
 
