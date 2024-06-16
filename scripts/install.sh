a331d951d38a9bddb9367dc912e8ef86377badd8
chan-nni <rkdcksal02@gmail.com>
2024-06-17 03:24:18 +0900

    install.sh 스크립트 수정

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,22 @@ echo "> Install: 서비스 유닛 파일 복사 및 등록"
 
 # 서비스 유닛 파일 복사
 SERVICE_FILE="/etc/systemd/system/cicd-test.service"
-cp $REPOSITORY/scripts/cicd-test.service $SERVICE_FILE
+
+# 서비스 유닛 파일 내용
+cat <<EOL | sudo tee $SERVICE_FILE
+[Unit]
+Description=cicd-test service
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/java -jar /home/ubuntu/mingle/build/libs/SNAPSHOT.jar
+User=ubuntu
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOL
 
 # 시스템d 데몬 리로드
 sudo systemctl daemon-reload
--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -6,6 +6,7 @@ echo "> ValidateService: 서비스 상태 검사 중..."
 
 # 서비스 유닛 파일 존재 여부 확인
 if systemctl list-units --type=service | grep -q "^$SERVICE_NAME.service"; then
+  echo "> $SERVICE_NAME 서비스 유닛 파일이 존재합니다."
   # 서비스 상태 확인
   if systemctl is-active --quiet $SERVICE_NAME; then
     echo "> $SERVICE_NAME 서비스가 실행 중입니다."
