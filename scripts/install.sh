925942644e0c80a6d5c190476b026beadfd91a8e
chan-nni <rkdcksal02@gmail.com>
2024-06-17 03:40:44 +0900

    appspec.yml 수정

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,22 +7,7 @@ echo "> Install: 서비스 유닛 파일 복사 및 등록"
 
 # 서비스 유닛 파일 복사
 SERVICE_FILE="/etc/systemd/system/cicd-test.service"
-
-# 서비스 유닛 파일 내용
-cat <<EOL | sudo tee $SERVICE_FILE
-[Unit]
-Description=cicd-test service
-After=network.target
-
-[Service]
-Type=simple
-ExecStart=/usr/bin/java -jar /home/ubuntu/mingle/build/libs/SNAPSHOT.jar
-User=ubuntu
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
-EOL
+cp $REPOSITORY/scripts/cicd-test.service $SERVICE_FILE
 
 # 시스템d 데몬 리로드
 sudo systemctl daemon-reload
@@ -32,3 +17,11 @@ sudo systemctl enable cicd-test.service
 sudo systemctl start cicd-test.service
 
 echo "> Install: cicd-test 서비스 등록 완료"
+
+# 서비스 상태 확인
+if systemctl is-active --quiet cicd-test; then
+  echo "> cicd-test 서비스가 성공적으로 시작되었습니다."
+else
+  echo "> cicd-test 서비스가 시작되지 않았습니다."
+  exit 1
+fi
