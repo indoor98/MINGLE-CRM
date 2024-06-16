3dc3ec67405d715c640855d538f771ce7cb83620
chan-nni <rkdcksal02@gmail.com>
2024-06-17 02:51:46 +0900

    appspec.yml - Hook 순서 변경

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SERVICE_NAME="cicd-test"  # 서비스 이름 설정
+SERVICE_NAME="cicd-test"
 
 echo "> ValidateService: 서비스 상태 검사 중..."
 
@@ -10,5 +10,6 @@ if systemctl is-active --quiet $SERVICE_NAME; then
   exit 0
 else
   echo "> $SERVICE_NAME 서비스가 실행되지 않거나 오류가 발생했습니다."
+  echo "> 서비스 상태 확인: $(systemctl status $SERVICE_NAME)"
   exit 1
 fi
