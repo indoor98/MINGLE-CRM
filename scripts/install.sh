0ff480553d526a9a8f390b922a2a42494ed6fc01
chan-nni <rkdcksal02@gmail.com>
2024-06-17 03:55:05 +0900

    install.sh 수정

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
 REPOSITORY="/home/ubuntu/mingle"
+SERVICE_FILE="/etc/systemd/system/cicd-test.service"
+
 cd $REPOSITORY
 
-echo "> Install: 서비스 유닛 파일 복사 및 등록"
+echo "> Install: 서비스 유닛 파일 생성 및 등록"
 
-# 서비스 유닛 파일 복사
-SERVICE_FILE="/etc/systemd/system/cicd-test.service"
-cp $REPOSITORY/scripts/cicd-test.service $SERVICE_FILE
+# 서비스 유닛 파일 내용 작성
+cat <<EOL | sudo tee $SERVICE_FILE
+[Unit]
+Description=cicd-test service
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/java -jar $REPOSITORY/build/libs/SNAPSHOT.jar
+User=ubuntu
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOL
 
 # 시스템d 데몬 리로드
 sudo systemctl daemon-reload
@@ -17,11 +31,3 @@ sudo systemctl enable cicd-test.service
 sudo systemctl start cicd-test.service
 
 echo "> Install: cicd-test 서비스 등록 완료"
-
-# 서비스 상태 확인
-if systemctl is-active --quiet cicd-test; then
-  echo "> cicd-test 서비스가 성공적으로 시작되었습니다."
-else
-  echo "> cicd-test 서비스가 시작되지 않았습니다."
-  exit 1
-fi
