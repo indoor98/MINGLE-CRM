b877b0a43481222b746075062969fb7802d8cf56
chan-nni <rkdcksal02@gmail.com>
2024-06-17 03:47:14 +0900

    validate_service.sh 수정

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 APP_NAME="cicd-test"
-SERVICE_FILE="$APP_NAME.service"
 
 echo "> ValidateService: 서비스 상태 검사 중..."
 
 # 서비스 유닛 파일 존재 여부 확인
-if systemctl list-units --type=service | grep -q "^$SERVICE_FILE$"; then
-  echo "> $SERVICE_FILE 서비스 유닛 파일이 존재합니다."
+if systemctl list-units --type=service | grep -q "^$APP_NAME.service"; then
+  echo "> $APP_NAME 서비스 유닛 파일이 존재합니다."
   # 서비스 상태 확인
   if systemctl is-active --quiet $APP_NAME; then
     echo "> $APP_NAME 서비스가 실행 중입니다."
@@ -18,6 +17,6 @@ if systemctl list-units --type=service | grep -q "^$SERVICE_FILE$"; then
     exit 1
   fi
 else
-  echo "> $SERVICE_FILE 서비스 유닛 파일이 없습니다."
+  echo "> $APP_NAME 서비스 유닛 파일이 없습니다."
   exit 1
 fi
