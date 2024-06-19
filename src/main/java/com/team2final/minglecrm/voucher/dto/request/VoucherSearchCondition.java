33960a771add03ed322044f81d76ca1aadac2558
dongwook <hsw50108@gmail.com>
2024-06-19 11:40:03 +0900

    feat : voucher 다중 검색

---
+++ src/main/java/com/team2final/minglecrm/voucher/dto/request/VoucherSearchCondition.java
@@ -0,0 +1,38 @@
+package com.team2final.minglecrm.voucher.dto.request;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class VoucherSearchCondition {
+
+    private LocalDateTime requestDate;
+
+    private String createdReason;
+
+    private LocalDateTime confirmDate;
+
+    private LocalDateTime conversionDate;
+
+    private String creatorName;
+
+    private Long confirmerId;
+
+    private String confirmerName;
+
+    private String customerName;
+
+    private String customerEmail;
+
+    private Long amount;
+
+    private String rejectedReason;
+
+    private String voucherCode;
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
+
+}
