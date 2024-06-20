fa29b22c864ed3d04fcd5a193a6f4d21d3ae3836
dongwook <hsw50108@gmail.com>
2024-06-21 00:01:44 +0900

    chore : close -> 닫기

--- src/pages/customer/detail/reservation/dining/DiningReservationDetail.vue
+++ src/pages/customer/detail/reservation/dining/DiningReservationDetail.vue
@@ -34,7 +34,7 @@
     </q-card-section>
 
     <q-card-actions align="right">
-      <q-btn flat label="Close" color="primary" @click="$emit('close')" />
+      <q-btn flat label="닫기" color="primary" @click="$emit('close')" />
     </q-card-actions>
   </q-card>
 </template>
