89d5c40f9436d6abacb6d1cae23bf32e6674e204
zzimni <hjm3327@naver.com>
2024-06-27 14:37:46 +0900

    바우처 관련 리팩토링 후 안쓰는 이전 파일 정리

--- src/pages/statistics/StatisticsPage.vue
+++ src/pages/statistics/StatisticsPage.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="statistics-page">
     <h1>Statistics Page</h1>
-    <!-- <BarChart /> -->
-    <!-- <DoughnutChart /> -->
-    <!-- <ReuseChart :updatedata="updatedata" /> -->
     <RevisitRate />
     <ReservationCntByYear />
     <ReservationCntMonthly />
@@ -13,15 +10,9 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
-import BarChart from "../../components/statistics/BarChart.vue";
-import DoughnutChart from "../../components/statistics/DoughnutChart.vue";
-import ReuseChart from "../../components/statistics/ReuseChart.vue";
 import RevisitRate from "../../components/statistics/RevisitRate.vue";
 import ReservationCntByYear from "../../components/statistics/ReservationCntByYear.vue";
 import ReservationCntMonthly from "../../components/statistics/ReservationCntMonthly.vue";
 import ReservationCntWeekly from "../../components/statistics/ReservationCntWeekly.vue";
 import VisitedCustomers from "../../components/statistics/VisitedCustomersByPeriod.vue";
-
-// const updatedata = ref([10, 20, 30, 40, 50]);
 </script>
