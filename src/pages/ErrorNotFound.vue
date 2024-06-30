8db6d5e67c6870b5642a53fa802e972a98dcb5aa
dongwook <hsw50108@gmail.com>
2024-07-01 00:44:47 +0900

    feat : error not found

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -1,16 +1,12 @@
 <template>
-  <div
-    class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center"
-  >
+  <div class="fullscreen bg-primary text-white text-center q-pa-md flex flex-center">
     <div>
-      <div style="font-size: 30vh">404</div>
-
-      <div class="text-h2" style="opacity: 0.4">Oops. Nothing here...</div>
-
+      <div style="font-size: 20vh">Mingle</div>
+      <div class="text-h2" style="opacity: 0.4">권한이 없거나 존재하지 않는 페이지입니다.</div>
       <q-btn
         class="q-mt-xl"
         color="white"
-        text-color="blue"
+        text-color="primary"
         unelevated
         to="/"
         label="Go Home"
@@ -25,3 +21,19 @@ defineOptions({
   name: "ErrorNotFound",
 });
 </script>
+
+<style scoped>
+$primary: #3C5B6E;
+
+.fullscreen {
+  background-color: $primary;
+}
+
+.q-btn {
+  transition: transform 0.3s;
+}
+
+.q-btn:hover {
+  transform: scale(1.1);
+}
+</style>
